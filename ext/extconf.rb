c0c1967f63bd7fb02e93301903b743d987cd2af5
Aman Gupta <aman@tmm1.net>
2011-02-14 02:48:52 -0800

    Simplify extconf.rb and make it work on OSX

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,32 +1,29 @@
 require "mkmf"
 require "fileutils"
 
-
-here = File.expand_path(File.dirname(__FILE__))
-udns_tarball = Dir.glob("#{here}/udns-*.tar.gz").first
-udns_path = udns_tarball.gsub(".tar.gz", "")
-
-Dir.chdir(here) do
-  puts(cmd = "tar xzf #{udns_tarball} 2>&1")
-  raise "'#{cmd}' failed" unless system(cmd)
-
-  Dir.chdir(udns_path) do
-    puts(cmd = "./configure 2>&1")
-    raise "'#{cmd}' failed" unless system(cmd)
-
-    puts(cmd = "make sharedlib 2>&1")
-    raise "'#{cmd}' failed" unless system(cmd)
-
-    puts(cmd = "ar r libudns.a *.lo 2>&1")
-    raise "'#{cmd}' failed" unless system(cmd)
-
-    FileUtils.mv "libudns.a", "../"
-    FileUtils.mv "udns.h", "../"
+def sys(cmd)
+  puts "  -- #{cmd}"
+  unless ret = xsystem(cmd)
+    raise "#{cmd} failed, please report to https://github.com/ibc/em-udns/issues"
   end
-
-  FileUtils.remove_dir(udns_path, force = true)
+  ret
 end
 
+here = File.expand_path('../', __FILE__)
+udns_tarball = Dir["#{here}/udns-*.tar.gz"].first
+udns_path = File.basename(udns_tarball, '.tar.gz')
+
+Dir.chdir(here) do
+  sys("tar zxvf #{udns_tarball}")
+
+  Dir.chdir(udns_path) do
+    sys("./configure")
+    sys("make libudns.a")
+
+    FileUtils.cp 'libudns.a', here
+    FileUtils.cp 'udns.h', here
+  end
+end
 
 have_library("udns")  # == -ludns
 create_makefile("em_udns_ext")
